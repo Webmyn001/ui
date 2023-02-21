de8dfa2b7b38697b07d0bd3e7863bd68432661bf
Webmyn001 <bellomuhyideen0001@gmail.com>
2023-02-21 11:57:31 -0800

    first init

---
+++ index.js
@@ -0,0 +1,48 @@
+const lessonRoutes = require("./Routes/lessonRoutes")
+const commentRoutes = require("./Routes/commentRoutes")
+
+
+const express = require("express")
+require("dotenv").config()
+const mongoose = require('mongoose')
+const bp = require('body-parser')
+const cors =require("cors") 
+
+const app = express()
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+app.use(cors());
+ const PORT= 4000
+app.use((req,res, next) => {
+   console.log(req.path , req.body ,req.method , res.body)
+   next()
+})
+
+
+
+
+
+mongoose.set('strictQuery', false);
+const connectDB = async ()=> {
+   try{
+      const conn = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`Mongodb connected :${conn.connection.host}`);
+
+   } catch (error){
+      console.log(error);
+      process.exit(1);
+   }
+}
+
+app.use("/api/lessons", lessonRoutes)
+app.use("/api/comment", commentRoutes)
+
+
+
+connectDB().then(()=> {
+   app.listen(PORT, ()=> {
+      console.log(`listening on port ${PORT}`)
+   })
+});
